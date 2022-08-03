7e25ce021d0b387f82d765b1339cdf0481e8ebba
juntak kim <junjun555@naver.com>
2022-08-03 17:46:37 +0900

    22/08/03-18:00

--- nono.py
+++ nono.py
@@ -1,45 +1,36 @@
 import random
 import numpy as np
 
+
+
 class LRWorld():
     
     def __init__(self):
         self.x=6
-        self.move_way = []
 
-    #상태전이형태 수정 필요
+    
     def step(self, a):
         global i 
         # 0번 액션: 왼쪽, 1번 액션: 오른쪽
-        print(self.move_way,i)
         if a==0:
-            
-            if self.move_way == [0,1,0,1,0]:
-                self.move_left()
-                reward = 1000 
-            else:
-                self.move_left()
-                reward = -1   #왼쪽으로 움직일 때 보상:-1
+            self.move_left()
+            reward = -1 #왼쪽으로 움직일 때 보상:-1
         elif a==1:
             self.move_right()
             reward = +1 #오른쪽으로 움직일 때 보상:+1
         i = i + 1
-        way = self.move_way
-        
         
         done = self.is_done()
         
-        return self.x, reward, way, done
+        return (self.x), reward, done
 
     def move_left(self):
-        self.x -= 1
-        self.move_way.append(0)  
+        self.x -= 1  
         if self.x < 0:
             self.x = 0
       
     def move_right(self):
         self.x += 1
-        self.move_way.append(1)
         if self.x > 12:
             self.x = 12
       
@@ -51,12 +42,12 @@ class LRWorld():
             return False
 
     def get_state(self):
-        return (self.x,self.move_way)
+        return (self.x)
       
     def reset(self):
         self.x = 6
-        self.move_way = []
-        return (self.x,self.move_way)
+        
+        return (self.x)
 
 class Agent():
     def __init__(self):
@@ -80,22 +71,20 @@ def main():
     
     alpha = 0.01
 
-    for k in range(2):
+    for k in range(10000):
         done = False
         global i
         i=0
         while not done:
-            x, way = env.get_state()
+            x = env.get_state()
             action = agent.select_action()
-            (x_prime), reward, way, done = env.step(action)
-            x_prime, way = env.get_state()
-            #print(way)
-            #print(data)
+            (x_prime), reward, done = env.step(action)
+            x_prime = env.get_state()
             data[x] = data[x] + alpha*(reward+gamma*data[x_prime]-data[x])
-            print("'",data)
+            
         env.reset()
             
-    
+    print(data)
 
 if __name__ == '__main__':
     main()
